6d8059901152ad764e77c932e937210afd51aa74
Azar <azar.barfipour@gmail.com>
2021-09-08 16:05:51 -0700

    creat js projects examples from coursera

---
+++ 2021-09/Ave/js/script.js
@@ -0,0 +1,7 @@
+function average(n,m){
+      ave = (n + m)/2;
+      return ave;
+}
+
+let a=average(50,82);
+console.log(a);
---
+++ 2021-09/threeFunctionDeclaration/script.js
@@ -0,0 +1,14 @@
+function power (x){
+   return x * x;
+}
+
+console.log(power(5));
+
+const power1= function (x){
+    return x * x;
+}
+console.log(power1(6));
+
+const power2 = x => x*x;
+console.log(power2(7));
+
