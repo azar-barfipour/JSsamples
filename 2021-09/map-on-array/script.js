8885602587401d8d5c8b67d682bd9a101b26c49f
Azar <azar.barfipour@gmail.com>
2021-09-19 14:21:45 -0700

    update map on arrays

--- 2021-09/map-on-array/script.js
+++ 2021-09/map-on-array/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+const users=[
+    {
+        firstName :'Azar',
+        lastName : 'Barfi',
+        age : 30
+    },
+    {
+        firstName :'Masoud',
+        lastName : 'Arefi',
+        age : 31
+    },
+    {
+        firstName :'Anna',
+        lastName : 'Barfi',
+        age : 35
+    }
+]
+
+const getUsers = users.map((user) => (user.age)
+)
+console.log(getUsers)
